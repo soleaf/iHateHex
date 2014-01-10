de7e04ab915120d39843459f0ec8c3f6a05520f8
soleaf <soleaf@gmail.com>
2014-01-10 18:31:14 +0900

    init

--- iHateHex/AppDelegate.m
+++ iHateHex/AppDelegate.m
@@ -7,13 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "MenuPopUpViewController.h"
-#import "AXStatusItemPopup.h"
-
-@interface AppDelegate () {
-    AXStatusItemPopup *_statusItemPopup;
-}
-@end
 
 @implementation AppDelegate
 
@@ -21,22 +14,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-    MenuPopUpViewController *contentViewController = [[MenuPopUpViewController alloc] initWithNibName:@"MenuPopUpViewController" bundle:nil];
-    
-    // init the status item popup
-    NSImage *image = [NSImage imageNamed:@"cloud"];
-    NSImage *alternateImage = [NSImage imageNamed:@"cloudgrey"];
-    
-    _statusItemPopup = [[AXStatusItemPopup alloc] initWithViewController:contentViewController image:image alternateImage:alternateImage];
-    
-    // globally set animation state (optional, defaults to YES)
-    //_statusItemPopup.animated = NO;
-    
-    //
-    // --------------
-    
-    // optionally set the popover to the contentview to e.g. hide it from there
-//    contentViewController.statusItemPopup = _statusItemPopup;
+   
 
 }
 @end
