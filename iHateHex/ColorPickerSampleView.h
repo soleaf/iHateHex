d5dee9116e6a5ce9f5ea1ea29748966e73c6e171
soleaf <soleaf@gmail.com>
2014-01-20 00:50:29 +0900

    New ColorPicker Appearance
    Add gride
    Simple color sample view

---
+++ iHateHex/ColorPickerSampleView.h
@@ -0,0 +1,15 @@
+//
+//  ColorPickerSampleView.h
+//  iHateHex
+//
+//  Created by soleaf on 1/20/14.
+//  Copyright (c) 2014 soleaf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ColorPickerSampleView : NSView
+
+-(void)setBackground:(NSColor *)aColor;
+
+@end
---
+++ iHateHex/ColorPickerSampleView.m
@@ -0,0 +1,53 @@
+//
+//  ColorPickerSampleView.m
+//  iHateHex
+//
+//  Created by soleaf on 1/20/14.
+//  Copyright (c) 2014 soleaf. All rights reserved.
+//
+
+#import "ColorPickerSampleView.h"
+
+@interface ColorPickerSampleView ()
+{
+    NSColor *background;
+}
+
+@end
+
+@implementation ColorPickerSampleView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+        self.layer = _layer;
+        self.wantsLayer = YES;
+        self.layer.masksToBounds = YES;
+        self.layer.cornerRadius = 10.0;
+        self.layer.borderColor = [NSColor blackColor].CGColor;
+        self.layer.borderWidth = 1.0;
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    [background set];
+    NSRectFill([self bounds]);
+
+}
+
+-(void)setBackground:(NSColor *)aColor
+{
+    if([background isEqual:aColor]) return;
+    background = aColor;
+    
+    //This is the most crucial thing you're missing: make the view redraw itself
+    [self setNeedsDisplay:YES];
+}
+
+@end
